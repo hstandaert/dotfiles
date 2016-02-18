1db5fe799c0cc52be56d924e3b0ff6ea454b8581
Helena Standaert <helena.standaert@gmail.com>
2016-02-18 14:25:51 +0100

    Add wb alias to the repo. This opens the current directory name in Google Chrome Canary. F.e. When you're in ~/Sites/project, http://project.dev will be opened

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,5 +29,6 @@ alias t="tree -L 1 -la"
 alias tt="tree -L 2 -la"
 alias td="tree -L 1 -la -d"
 
-#PhpStorm
+#Applications
 alias strm="open -a PhpStorm"
+alias wb="open -a 'Google Chrome Canary' http://${PWD##*/}.dev" # Open the current directory name in Google Chrome Canary. F.e. When you're in ~/Sites/project, http://project.dev will be opened
