3b4e51794b8724cc1d2012676217fbaefc78bc14
Helena Standaert <helena.standaert@gmail.com>
2020-10-21 11:48:41 +0200

    Update git configuration

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,16 +1,5 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
-# main alias
-alias g='git'
-
-# The rest of my fun git aliases
-alias gl='git up'
+# Fun git aliases
+alias gl='git pull --stat'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
@@ -27,10 +16,6 @@ alias gr='git reset'
 alias grh='git reset HEAD~1'
 alias gua='git reset HEAD' # git undo add
 alias gum='git reset --merge ORIG_HEAD' # git undo merge
-
-# Git flow
-alias gf='git flow'
-alias gff='git flow feature'
 
 # Gitio urls
 gitio() {
