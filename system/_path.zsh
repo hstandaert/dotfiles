4943cfe7df9fc810780b83e6edf80acde040059e
Helena Standaert <helena.standaert@gmail.com>
2020-10-21 11:37:22 +0200

    Update path variables

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,9 +1,6 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.gem/ruby/2.0.0/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-# add rbenv to PATH
-export PATH="$HOME/.rbenv/bin:$PATH"
-
 # add composer to PATH
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
@@ -12,7 +9,14 @@ export NVM_DIR="$HOME/.nvm"
 source $(brew --prefix nvm)/nvm.sh
 
 # add gems to path
-export PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 
 # add mysql dir to path
 export PATH="/usr/local/mysql/bin:$PATH"
+
+# ANDROID_HOME
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
